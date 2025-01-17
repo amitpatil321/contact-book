57b4013d8c57c8e727aaf0d1a0b3d94bd2b94644
Amit <amitpatil321@gmail.com>
2025-01-18 00:25:46 +0530

    feat: favorite feature implemented in contact details page

--- src/components/ContactDetails/ActionButtons.tsx
+++ src/components/ContactDetails/ActionButtons.tsx
@@ -1,10 +1,23 @@
 import { Button } from "primereact/button";
-import { Contact } from "../../types/types";
+import { useContext } from "react";
+import { AppContext } from "../../context/AppContext";
+import { AppContextType, Contact } from "../../types/types";
 
 const ActionButton: React.FC<{ contact: Contact }> = ({ contact }) => {
+  const { favoritesArr, favLoading, handleFavorites } = useContext(
+    AppContext
+  ) as AppContextType;
+
+  console.log(favoritesArr, favLoading);
+
   return (
     <div className="flex justify-start items-center gap-2 mt-auto text-gray-400 text-sm">
-      <span className="hover:text-pink-700 cursor-pointer">Favorite</span>
+      <span
+        className="hover:text-pink-700 cursor-pointer"
+        onClick={(event) => handleFavorites(event, contact?.id)}
+      >
+        {favoritesArr?.includes(contact.id) ? "UnFavorite" : "Favorite"}
+      </span>
       <span className="text-gray-400">|</span>
       <span className="hover:text-purple-500 cursor-pointer">Edit</span>
       <span className="text-gray-400">|</span>
