8390b8551956e56b1cc5152ee31ed88d645f52ec
Amit <amitpatil321@gmail.com>
2025-01-21 17:17:31 +0530

    feat: not found page ui change

--- src/components/NotFound.tsx
+++ src/components/NotFound.tsx
@@ -1,7 +1,8 @@
 const NotFound = () => {
   return (
-    <div className="flex justify-center items-center bg-white p-4 pt-3 rounded-lg w-full h-[calc(100vh-110px)] overflow-y-auto">
-      <h2>404 - Page not found!</h2>
+    <div className="flex flex-col justify-center items-center gap-6 bg-white p-4 pt-3 rounded-lg w-full h-[calc(100vh-110px)] text-purple-600 overflow-y-auto">
+      <h1 className="!font-light !text-8xl">404</h1>
+      <h3 className="!font-light !text-5xl">Page not found!</h3>
     </div>
   );
 };
