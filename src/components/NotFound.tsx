2fe1888d2d83b71ca5d6deb49eeec34b4bb81aee
Amit <amitpatil321@gmail.com>
2025-01-21 11:28:32 +0530

    feat: add 404 page

---
+++ src/components/NotFound.tsx
@@ -0,0 +1,9 @@
+const NotFound = () => {
+  return (
+    <div className="flex justify-center items-center bg-white p-4 pt-3 rounded-lg w-full h-[calc(100vh-110px)] overflow-y-auto">
+      <h2>404 - Page not found!</h2>
+    </div>
+  );
+};
+
+export default NotFound;
--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -1,6 +1,7 @@
 import { Route, Routes } from "react-router";
 import App from "../App";
 import Layout from "../components/Layout/Layout";
+import NotFound from "../components/NotFound";
 import { PAGES } from "../constants/constants";
 import Archived from "../pages/Archived";
 import Deleted from "../pages/Deleted";
@@ -9,11 +10,12 @@ import Favorites from "../pages/Favorites";
 const AppRoutes = () => {
   return (
     <Routes>
-      <Route element={<Layout />}>
+      <Route path="/" element={<Layout />}>
         <Route index element={<App />} />
         <Route path={PAGES.favorites} element={<Favorites />} />
         <Route path={PAGES.archived} element={<Archived />} />
         <Route path={PAGES.deleted} element={<Deleted />} />
+        <Route path="*" element={<NotFound />} />
       </Route>
     </Routes>
   );
