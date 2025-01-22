f59813c808ded21c5ede9d6d88e13670ecf25443
Amit <amitpatil321@gmail.com>
2025-01-22 13:02:55 +0530

    feat: clear selected contact on the route change

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,16 @@
+import React from "react";
+import { useLocation } from "react-router";
 import "./App.css";
 import AppRoutes from "./routes/routes";
+import useStore from "./store/store";
 
 function App() {
+  const { setSelectedContact } = useStore();
+  const location = useLocation();
+  React.useEffect(() => {
+    setSelectedContact(null);
+  }, [location, setSelectedContact]);
+
   return <AppRoutes />;
 }
 
