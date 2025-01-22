cc34b75d3b50b186d17ce5df40a69a71074c84de
Amit <amitpatil321@gmail.com>
2025-01-22 12:45:23 +0530

    feat: speed improvement

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -1,23 +1,28 @@
+import { lazy, Suspense } from "react";
 import { Route, Routes } from "react-router";
+
 import Layout from "../components/Layout/Layout";
 import NotFound from "../components/NotFound";
 import { PAGES } from "../constants/constants";
-import Active from "../pages/Active";
-import Archived from "../pages/Archived";
-import Deleted from "../pages/Deleted";
-import Favorites from "../pages/Favorites";
+
+const Active = lazy(() => import("../pages/Active"));
+const Archived = lazy(() => import("../pages/Archived"));
+const Deleted = lazy(() => import("../pages/Deleted"));
+const Favorites = lazy(() => import("../pages/Favorites"));
 
 const AppRoutes = () => {
   return (
-    <Routes>
-      <Route path="/" element={<Layout />}>
-        <Route index element={<Active type="active" />} />
-        <Route path={PAGES.favorites} element={<Favorites />} />
-        <Route path={PAGES.archived} element={<Archived type="archived" />} />
-        <Route path={PAGES.deleted} element={<Deleted type="deleted" />} />
-        <Route path="*" element={<NotFound />} />
-      </Route>
-    </Routes>
+    <Suspense fallback={<div>Loading...</div>}>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Active type="active" />} />
+          <Route path={PAGES.favorites} element={<Favorites />} />
+          <Route path={PAGES.archived} element={<Archived type="archived" />} />
+          <Route path={PAGES.deleted} element={<Deleted type="deleted" />} />
+          <Route path="*" element={<NotFound />} />
+        </Route>
+      </Routes>
+    </Suspense>
   );
 };
 
