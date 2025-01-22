7452f8e77ee1d16078281c85e9dff24b371485b1
Amit <amitpatil321@gmail.com>
2025-01-22 12:54:40 +0530

    feat: on search icon click focus input

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,6 @@
 import { Button } from "primereact/button";
 import { InputText } from "primereact/inputtext";
-import { useContext } from "react";
+import { useContext, useRef } from "react";
 import { AppContext } from "../context/AppContext";
 import { AppContextType } from "../types/types";
 
@@ -8,25 +8,30 @@ const Header: React.FC = () => {
   const { expanded, setExpanded, setShowAddContact } = useContext(
     AppContext
   ) as AppContextType;
+  const inputRef = useRef<HTMLInputElement>(null);
 
   return (
     <div className="flex items-center bg-white p-4 rounded-lg w-full h-16">
       <div className="w-[60%] p-inputgroup">
         <Button
-          icon="pi pi-bars border-transparent focus:border-transparent !outline-none focus:ring-0 "
+          icon="pi pi-bars"
           text
           onClick={() => setExpanded(!expanded)}
           aria-label="Open navigation menu"
+          className="border-transparent focus:border-transparent focus:ring-0 !outline-none"
         />
         <Button
-          icon="pi pi-search border-transparent focus:border-transparent text-purple-600 bold focus:ring-0"
+          icon="pi pi-search"
           text
           aria-label="search contact"
+          className="bg-transparent border-transparent focus:border-transparent focus:ring-0 text-purple-600 cursor-default !outline-none bold"
+          onClick={() => inputRef?.current?.focus()}
         />
         <label htmlFor="search-contact" className="sr-only">
           Search contact
         </label>
         <InputText
+          ref={inputRef}
           placeholder="Search contact..."
           className="pl-0 border-transparent focus:border-transparent border-none focus:ring-0"
         />
