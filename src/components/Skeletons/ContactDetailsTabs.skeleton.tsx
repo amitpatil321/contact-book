b03becf64c0e0edd12a235e7acf069343ebaa73e
Amit <amitpatil321@gmail.com>
2025-01-22 13:09:20 +0530

    feat: adjust skeleton

--- src/components/Skeletons/ContactDetailsTabs.skeleton.tsx
+++ src/components/Skeletons/ContactDetailsTabs.skeleton.tsx
@@ -3,18 +3,24 @@ import { Skeleton } from "primereact/skeleton";
 const ContactDetailsTabs = () => {
   return (
     <>
-      <div className="flex gap-6 mt-14 pb-2 pl-2">
+      <div className="flex gap-6 pb-2 pl-2">
         <Skeleton width="8rem" height="2rem"></Skeleton>
         <Skeleton width="8rem" height="2rem"></Skeleton>
         <Skeleton width="8rem" height="2rem"></Skeleton>
         <Skeleton width="7rem" height="2rem"></Skeleton>
       </div>
       <hr />
-      <div className="flex flex-col pt-6 pl-4">
-        <Skeleton width="8rem" height="0.6rem" className="mb-2"></Skeleton>
-        <Skeleton width="5rem" height="0.6rem" className="mb-2"></Skeleton>
-        <Skeleton width="7rem" height="0.6rem" className="mb-2"></Skeleton>
-        <Skeleton width="5rem" height="0.6rem" className="mb-2"></Skeleton>
+      <div className="flex flex-row gap-2 pt-6 pl-6">
+        <div>
+          <Skeleton width="5rem" height="0.6rem" className="mb-2"></Skeleton>
+          <Skeleton width="6rem" height="0.6rem" className="mb-2"></Skeleton>
+          <Skeleton width="6rem" height="0.6rem" className="mb-2"></Skeleton>
+        </div>
+        <div>
+          <Skeleton width="5rem" height="0.6rem" className="mb-2"></Skeleton>
+          <Skeleton width="4rem" height="0.6rem" className="mb-2"></Skeleton>
+          <Skeleton width="6rem" height="0.6rem" className="mb-2"></Skeleton>
+        </div>
       </div>
     </>
   );
